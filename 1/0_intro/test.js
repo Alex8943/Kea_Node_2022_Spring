9e60abd71ef7fd7d235324467b95e67a0b0e29c8
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2022-02-01 10:44:55 +0100

    add a test.js folder

---
+++ 1/0_intro/test.js
@@ -0,0 +1,11 @@
+const name = "Alexander";
+console.log("Hello world ", name);
+
+//Forstå værdien af en variabel 
+
+//"==" vs "===" forskellen er man tjekker ikke ordentligt med kun 2 "=" . Brug i stedet 3. 
+
+console.log(2 === "2");
+
+
+//Ikke brug "git add ." mere
